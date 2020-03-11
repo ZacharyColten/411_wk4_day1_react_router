98f968e2fbaabac48b7bdf919438f1aa35463f28
zacharycolten <zacharycolten@gmail.com>
2020-03-11 12:38:01 -0500

    turn-in ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import './App.css'
+import Router from './Router';
 // Write imports for Router & BrowserRouter here //
 
 function App() {
   return (
+    <BrowserRouter>
     <Navigation />
+    <Router/>
+    </BrowserRouter>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card, CardContent, CardActions, Divider } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 import cars from '../cars.json'
 
 const Home = () => {
@@ -11,16 +12,16 @@ const Home = () => {
                     <CardContent className="text-gray">
                         <span>{car.Name.toUpperCase()}</span>
                         <ul>
-                        <li>Miles_per_Gallon: {car["Miles_per_Gallon"]}</li>
-                        <li>Cylinders: {car["Cylinders"]}</li>
-                        <li>Displacement: {car["Displacement"]}</li>
-                        <li>Horsepower: {car["Horsepower"]}</li>
+                            <li>Miles_per_Gallon: {car["Miles_per_Gallon"]}</li>
+                            <li>Cylinders: {car["Cylinders"]}</li>
+                            <li>Displacement: {car["Displacement"]}</li>
+                            <li>Horsepower: {car["Horsepower"]}</li>
                         </ul>
                     </CardContent>
                     <Divider />
                     <CardActions style={{ color: 'mediumblue' }}>
-                        {/* Change a tag to Link */}
-                        <a>See more Details</a>
+                        <Link to={`/car/${car.id}`}>
+                            <a>Click here to see more details</a></Link>
                     </CardActions>
                 </Card>
             ))}
